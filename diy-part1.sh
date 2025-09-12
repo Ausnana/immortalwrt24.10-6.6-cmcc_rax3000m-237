b1f9ccada9ad9240c159751e80e77f301f59de57
Ausnana <120234290+Ausnana@users.noreply.github.com>
2025-09-12 19:04:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
